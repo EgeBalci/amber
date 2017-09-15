a44eeeff185db84e6fb50a69cf8575b7d5218167
EgeBalci <egebal1996@gmail.com>
2017-09-15 19:09:08 +0300

    - SEH record fix method revoked

--- src/compile.go
+++ src/compile.go
@@ -8,26 +8,26 @@ func compile() {
   verbose("[*] Ciphering payload...",BoldYellow)
   crypt() // 4 steps
 
-  xxd := exec.Command("sh", "-c", "rm Payload && mv Payload.xor Payload && xxd -i Payload > Stub/PAYLOAD.h")
+  xxd := exec.Command("sh", "-c", "rm Payload && mv Payload.xor Payload && xxd -i Payload > stub/PAYLOAD.h")
   xxd.Stdout = os.Stdout
   xxd.Stderr = os.Stderr
   xxd.Run()
   progress()
 
-  _xxd := exec.Command("sh", "-c", "xxd -i Payload.key > Stub/KEY.h")
+  _xxd := exec.Command("sh", "-c", "xxd -i Payload.key > stub/KEY.h")
   _xxd.Stdout = os.Stdout
   _xxd.Stderr = os.Stderr
   _xxd.Run()
   progress()
 
-  mingwObj, mingwObjErr := exec.Command("sh", "-c", "i686-w64-mingw32-g++-win32 -c Stub/Stub.cpp").Output()
+  mingwObj, mingwObjErr := exec.Command("sh", "-c", "i686-w64-mingw32-g++-win32 -c stub/stub.cpp").Output()
   ParseError(mingwObjErr,"\n[!] ERROR: While compiling the object file.",string(mingwObj))
   
   progress()
 
-  var compileCommand string = "i686-w64-mingw32-g++-win32 Stub.o "
+  var compileCommand string = "i686-w64-mingw32-g++-win32 stub.o "
   if peid.resource == true {
-    compileCommand += "Stub/Resource.o "
+    compileCommand += "stub/Resource.o "
   }
   if peid.subsystem == 2 { // GUI
     compileCommand += string("-mwindows -o "+peid.fileName)
